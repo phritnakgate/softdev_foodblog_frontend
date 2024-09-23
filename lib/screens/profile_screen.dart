636039bd3afaa5aca756e246aa93dfa7ce6d8c4f
Vivat Techakosol <nickvivat@hotmail.com>
2024-09-23 16:53:47 +0700

    Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:softdev_foodblog_frontend/widgets/profile_screen/switch_widget.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool showPosts = true;
+
+  // Example data for "My Posts" and "Liked Posts"
+  List<String> myPosts = ['My Post 1', 'My Post 2', 'My Post 3', 'My Post 4'];
+  List<String> likedPosts = ['Liked Post 1', 'Liked Post 2'];
+
+  // Example tags
+  List<String> tags = ['Breakfast', 'Lunch', 'Dinner'];
+
+  @override
   Widget build(BuildContext context) {
+    List<String> currentList = showPosts ? myPosts : likedPosts;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Center(child: Text('Profile')),
-        backgroundColor: Colors.orangeAccent, // Customize the AppBar color
+        title: const Center(
+          child: Text(
+            'Profile',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.orangeAccent,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
@@ -30,44 +55,39 @@ class ProfileScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 10),
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'My Posts',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+          SwitchWidget(
+            showPosts: showPosts,
+            onSwitch: (bool value) {
+              setState(() {
+                showPosts = value;
+              });
+            },
           ),
           const SizedBox(height: 10),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: GridView.builder(
-                itemCount: 20, // Number of posts
+                itemCount: currentList.length,
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 1,
                   mainAxisSpacing: 6,
-                  mainAxisExtent: 120,
+                  mainAxisExtent: 140,
                 ),
                 itemBuilder: (BuildContext context, int index) {
                   return Container(
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: Colors.orangeAccent,
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.grey.withOpacity(0.2),
-                              spreadRadius: 1,
-                              blurRadius: 2,
-                              offset: const Offset(0, 3))
-                        ]),
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.orangeAccent,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 2,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
@@ -77,26 +97,49 @@ class ProfileScreen extends StatelessWidget {
                           height: 150,
                         ),
                         const SizedBox(width: 20),
-                        const Expanded(
+                        Expanded(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Padding(
-                                padding: EdgeInsets.only(bottom: 8.0),
-                                child: Text(
-                                  'burger',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+                                    child: Text(
+                                      showPosts ? 'My Post ${index + 1}' : 'Liked Post ${index + 1}',
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 10.0),
+                                    child: IconButton(
+                                      onPressed: () {
+                                       
+                                      },
+                                      icon: const Icon(Icons.edit),
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 20),
+                              ElevatedButton(
+                                onPressed: () {
+                                  // Handle tag button click
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.white,
+                                  foregroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18),
                                   ),
                                 ),
-                              ),
-                              Text(
-                                'description',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                ),
+                                child: Text(tags[index % tags.length]),
                               ),
                             ],
                           ),
---
+++ lib/widgets/profile_screen/switch_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class SwitchWidget extends StatelessWidget {
+  final bool showPosts;
+  final ValueChanged<bool> onSwitch;
+
+  const SwitchWidget({
+    super.key,
+    required this.showPosts,
+    required this.onSwitch,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () => onSwitch(true),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: showPosts ? Colors.orangeAccent : Colors.transparent,
+                  width: 3,
+                ),
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+            child: Text(
+              'My Posts',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: showPosts ? FontWeight.bold : FontWeight.normal,
+                color: showPosts ? Colors.orangeAccent : Colors.grey,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(width: 20),
+        GestureDetector(
+          onTap: () => onSwitch(false),
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: showPosts ? Colors.transparent : Colors.orangeAccent,
+                  width: 3,
+                ),
+              ),
+            ),
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+            child: Text(
+              'Liked Posts',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: showPosts ? FontWeight.normal : FontWeight.bold,
+                color: showPosts ? Colors.grey : Colors.orangeAccent,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
